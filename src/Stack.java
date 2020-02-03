d14fed6c382525818526df0ff9bb50659b869748
IngeborRV <47764062+ingebor@users.noreply.github.com>
2020-02-02 18:16:32 -0600

    para que pueda trabajar en las clases

--- src/Stack.java
+++ src/Stack.java
@@ -1,25 +1,35 @@
 
-	public interface Stack<E> 
-	{
 
-	   public void push(E item);
-	   // pre: 
-	   // post: item is added to stack
-	   // will be popped next if no intervening push
-	   
-	   public E pop();
-	   // pre: stack is not empty
-	   // post: most recently pushed item is removed and returned
-	   
-	   public E peek();
-	   // pre: stack is not empty
-	   // post: top value (next to be popped) is returned
-	   
-	   public boolean empty();
-	   // post: returns true if and only if the stack is empty
-	   
-	   public int size();
-	   // post: returns the number of elements in the stack
-
+public class Stack<E> implements iStack<E>{
+	protected Arraylist<E> lista;
+	
+	public StackArraylist() {
+		lista = new Arraylist<E>();
+		
+	}
+	
+	public void push(E item){
+		lista.add(item);
 	}
 
+	public E pop(){
+
+		return lista.remove(size()-1);
+	}
+
+	public E peek(){
+
+		return data.get(size()-1);
+	}
+
+	public boolean empty(){
+
+		return size()==0;
+	}
+
+	public int size(){
+
+		return lista.size();
+	}
+	   
+}

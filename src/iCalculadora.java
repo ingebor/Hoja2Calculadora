847a9f3f64c233ae03348642892505b2b40c58a9
IngeborRV <47764062+ingebor@users.noreply.github.com>
2020-02-02 18:16:53 -0600

    para que pueda trabajar en las clases x2

---
+++ src/iCalculadora.java
@@ -0,0 +1,11 @@
+
+
+public interface iCalculadora{
+	public int sumar(int operando1, int operando2);
+
+	public int restar(int operando1, int operando2);
+
+	public int dividir(int operando1, int operando2);
+
+	public int multiplicar(int operando1, int operando2);
+}

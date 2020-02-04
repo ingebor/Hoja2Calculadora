a19326ce571dd3caef3fb8e8eab9336a166f9993
Requetetin <martinamadog@gmail.com>
2020-02-03 18:27:52 -0600

    Java doc para iCalculadora

--- src/iCalculadora.java
+++ src/iCalculadora.java
@@ -6,10 +6,19 @@
 
 public interface iCalculadora{
 	public int sumar(int operando1, int operando2);
+	// pre: at least two numbers exist in stack used
+	// post: returns the sum of the numbers
 
 	public int restar(int operando1, int operando2);
+	// pre: at least two numbers exist in stack used
+	// post: returns the difference of the numbers 
 
 	public int dividir(int operando1, int operando2);
+	// pre: at least two numbers exist in stack used  
+	// post: Divides both numbers 
+	// Cant divide by zero, and doesnt return decimal places
 
 	public int multiplicar(int operando1, int operando2);
+	// pre: at least two numbers exist in stack used  
+	// post: multiplies both numbers
 }
